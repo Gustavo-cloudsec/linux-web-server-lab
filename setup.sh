6cff95041a38cf615ae335841e0c6b9639ab800b
Gustavo <gh063785@gmail.com>
2026-04-20 22:11:50 -0300

    feat: add firewall configuration (ufw)

--- setup.sh
+++ setup.sh
@@ -9,3 +9,11 @@ sudo apt install nginx -y
 echo "Instalando curl..."
 sudo apt install curl -y
 
+echo "Configurando firewall..."
+
+sudo apt install ufw -y
+sudo ufw default deny incoming
+sudo ufw defalt allow outgoing
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw --force enable
